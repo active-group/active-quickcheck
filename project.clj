41610dd4e519fd2e884d7025cb2d427efcb7a763
Mike Sperber <sperber@deinprogramm.de>
2013-03-15 09:44:35 +0100

    Add codox to project.clj.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "QuickCheck clone for Clojure"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/algo.monads "0.1.4"]])
+                 [org.clojure/algo.monads "0.1.4"]]
+  :plugins [[codox "0.6.4"]])
 
