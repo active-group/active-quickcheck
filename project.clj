24d4f29f304f0589fca6ad7b7bee8bffbc0d90d9
Johannes Maier <johannes.maier@active-group.de>
2018-11-29 09:47:10 +0100

    Upgrade to latest version of clojure.math.numeric-tower

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "QuickCheck clone for Clojure"
   :url "http://github.com/active-group/active-quickcheck"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/test.check "0.9.0"]
                  [active-clojure "0.26.0"]]
   :plugins [[codox "0.6.4"]]
