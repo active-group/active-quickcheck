97c6503b9637f7f7836ddbbb40f7f61c3d1c4717
Marcus Crestani <marcus@crestani.de>
2019-05-15 15:08:47 +0200

    Release 0.6.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject active-quickcheck "0.6.0-SNAPSHOT"
+(defproject active-quickcheck "0.6.0"
   :description "QuickCheck clone for Clojure"
   :url "http://github.com/active-group/active-quickcheck"
   :dependencies [[org.clojure/clojure "1.9.0"]
