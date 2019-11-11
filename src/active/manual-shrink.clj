71bdec770fe856ee2f426113651c48c935bc32cd
Florian Engel <florianengel39@gmail.com>
2019-11-11 12:08:44 +0100

    Change manual shrink to use multiple arguments

--- src/active/manual-shrink.clj
+++ src/active/manual-shrink.clj
@@ -5,9 +5,7 @@
 (defn result-mapped
   "Monoidal plus of result."
   [result1 result2]
-  (println "result-mapped result1" (pr-str result1))
   (assert (qc/check-result? result1))
-  (println "result-mapped result2" (pr-str result2))
   (assert (qc/check-result? result2))
   (cond
     (qc/check-result-ok result1) result2
@@ -17,69 +15,68 @@
   [res arg]
   (assert (qc/check-result? res))
   (cond
-    (empty? (qc/check-result-arguments-list res)) (assoc res :arguments-list [arg])
+    (empty? (qc/check-result-arguments-list res)) (assoc res :arguments-list arg)
     :else res))
+
+(defn cartesian-product [colls]
+  (if (empty? colls)
+    [[]]
+    (for [more (cartesian-product (rest colls))
+          x (first colls)]
+      (cons x more))))
 
 (defn find-failing
   [smaller func]
-  (let [_ (println (pr-str "find-failing smaller:" smaller))])
-  (let [_ (println (pr-str "find-failing func smaller:" (mapv func smaller)))])
-  (let [_ (println (pr-str "find-failing res-gen func smaller:" (mapv qc/coerce->result-generator (mapv func smaller))))])
+  (let [args (cartesian-product smaller)]
   (monad/monadic
-   [results (monad/sequ (mapv qc/coerce->result-generator (mapv func smaller)))]
-   (let [_ (println (pr-str "find-failing results:" results))
-         failingResults (filter (fn [[_ result]] (println "result:" (pr-str result)) (not (qc/check-result-ok result)))
-                                (mapv vector smaller results))])
+   [results (monad/sequ (mapv qc/coerce->result-generator (mapv (partial apply func) args)))]
+   (let [failingResults (filter (fn [[_ result]] (not (qc/check-result-ok result)))
+                                (mapv vector args results))])
    (monad/return
     (cond
       (empty? failingResults) :no-failing-result
-      :else (first failingResults)))))
+      :else (first failingResults))))))
 
 (defn shrinking
   "
-  Apply shrink to arg and find failing result in the resulting list
-  recursive call shrinking as long as there is a failing result
+  Apply shrinks to args and find failing result in the resulting list
+  recursive call shrinking as long as there is a failing result.
+  For every arg there has to be a shrink function
   "
-  [shrink arg-name arg func fuel]
-  (let [_ (println "shrinking arg-name:" arg-name)])
-  (let [_ (println (pr-str "shrinking arg:" arg))])
-  (let [_ (println (pr-str "shrinking fuel:" fuel))])
-  (let [children (shrink arg)
-        _ (println "shrinking children: " (pr-str children))]
+  [shrinks arg-names args func fuel]
+  (assert (= (count shrinks) (count args))
+          "Number of shrink functions does not match number of arguments")
+  (assert (= (count arg-names) (count args))
+          (concat "Number of arg-name("
+                  (pr-str (count shrinks))
+                  ") does not match number of arguments("
+                  (pr-str (count shrinks))
+                  ")"))
+  (let [children (mapv (fn [shrink arg] (shrink arg)) shrinks args)]
     (monad/monadic
      [maybeFailingResult (find-failing children func)]
-     (let [_ (println "shrinking maybeFailingResult:" (pr-str maybeFailingResult))])
      (cond
        (or (= maybeFailingResult :no-failing-result)
            (<= fuel 0)) (monad/return (assoc qc/nothing :ok true))
        :else (monad/monadic
-              (let [[shrunk, failure] maybeFailingResult])
-              [result (shrinking shrink arg-name shrunk func (- fuel 1))]
+              (let [[shrunks, failure] maybeFailingResult])
+              [result (shrinking shrinks arg-names shrunks func (- fuel 1))]
               (monad/return
                (result-add-argument-if-empty (result-mapped result failure)
-                                                       (vector arg-name shrunk))))))))
-
+                                                       (mapv vector arg-names shrunks))))))))
 
 (defn for-all-with-shrink-with-name
   "Bind name to generated value, try to shrink, supplying informative name.,"
-  [func arg-name arg shrink]
-  (let [_ (println (pr-str "for-all-with-shrink arg-name:" arg-name))])
-  (let [_ (println (pr-str "for-all-with-shrink arg:" arg))])
+  [func arg-names args shrink]
+  (assert (= (count arg-names) (count args))
+          "Number of arg-names does not match number of arguments")
   (monad/monadic
-   [arg (qc/coerce->generator arg)
-    res (qc/coerce->result-generator (func arg))]
-   (let [_ (println (pr-str "for-all-with-shrink arg:" arg))])
-   (let [_ (println (pr-str "for-all-with-shrink res:" res))])
+   [args (monad/sequ (map qc/coerce->generator args))
+    res (qc/coerce->result-generator (apply func args))
+    shrunken (shrinking shrink arg-names args func 20)]
+   (let [result (qc/result-add-arguments res [(vector arg-names args)])])
+   ; TODO is this lazy. Check efficiency
    (cond
-      (qc/check-result-ok res) (monad/return (qc/result-add-arguments res [(vector arg-name arg)]))
-      :else (shrinking shrink arg-name arg func 20))))
-
-;(generate 5 my-gen (for-all-with-shrink-with-name (fn [x] (or (= x 0) (not= x 0)))
-;                                                  "x"
-;                                                  arbitrary-int
-;                                                  (fn [x] [(- x 1)])))
-;(generate 5 my-gen (for-all-with-shrink-with-name (partial = 0)
-;                                                  "x"
-;                                                  arbitrary-int
-;                                                  (fn [x] [(- x 1000000)])))
-
+      (qc/check-result-ok result) (monad/return result)
+      (qc/check-result-ok shrunken) (monad/return result)
+      :else (monad/return shrunken))))
