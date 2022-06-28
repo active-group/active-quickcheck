2e5a7c76646953e5479e1d09d15ea7dd8767555a
Marcus Crestani <marcus.crestani@active-group.de>
2022-06-28 15:26:17 +0200

    Release 0.8.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/active-quickcheck "0.8.0-SNAPSHOT"
+(defproject de.active-group/active-quickcheck "0.8.0"
   :description "QuickCheck clone for Clojure"
   :url "http://github.com/active-group/active-quickcheck"
   :dependencies [[org.clojure/clojure "1.10.0"]
